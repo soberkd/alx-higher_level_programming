ea092b1c9e88884c644543b153acfcb543572ac6
soberkd <soberkd6@gmail.com>
2022-11-29 21:34:47 +0000

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            x = 'FizzBuzz'
+        elif x % 5 == 0:
+            x = 'Buzz'
+        elif x % 3 == 0:
+            x = 'Fizz'
+        print("{}".format(x), end=" ")
