3252133ddc41b0f80e413f88ece4d358a06a9f87
soberkd <soberkd6@gmail.com>
2022-12-01 14:06:00 +0000

    import calculator fuctions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     division = div(a, b)
 
     print("{} + {} = {}".format(a, b, addition))
-    print("{} + {} = {}".format(a, b, subtraction))
-    print("{} + {} = {}".format(a, b, multiplication))
-    print("{} + {} = {}".format(a, b, division))
+    print("{} - {} = {}".format(a, b, subtraction))
+    print("{} * {} = {}".format(a, b, multiplication))
+    print("{} / {} = {}".format(a, b, division))
