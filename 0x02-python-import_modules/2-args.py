caa856c519681b35194a9b8bfea7e6e1d1189475
soberkd <soberkd6@gmail.com>
2022-12-01 12:53:35 +0000

    prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+length = len(sys.argv) - 1
+if length == 0:
+    print("{} arguments.".format(length))
+elif length == 1:
+    print("{} argument:".format(length))
+    print("{}: {}".format(length, sys.argv[1]))
+else:
+    print("{} arguments:".format(length))
+    for i in range(length):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
