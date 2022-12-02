8710306fb8ac359b2c19dae3ff77a82c71d91964
soberkd <soberkd6@gmail.com>
2022-12-02 09:41:57 +0000

    Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,9 @@
 
 
 def print_list_integer(my_list=[]):
+    """Prints List of integers in a list
+    Args:
+        my_list: list of integers
+    """
     for integer in my_list:
         print("{}".format(integer))
