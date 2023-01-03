69ce14ec8a1d512cb716d085997eae7dbce09804
soberkd <soberkd6@gmail.com>
2023-01-03 15:54:17 +0000

    Detect instance deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Instantiation with optional width and height"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        string = ''
+        if self.__width == 0 or self.__height == 0:
+            return string
+        for x in range(0, self.__height):
+            if x != self.__height - 1:
+                string = string + "#" * self.__width + "\n"
+            else:
+                string = string + "#" * self.__width
+        return string
+
+    def __repr__(self):
+        return "Rectangle(" + str(self.__width) + ", " + \
+                            str(self.__height) + ")"
+
+    def __del__(self):
+        print("Bye rectangle...")
