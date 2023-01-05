9b6ad8b8d705d9605d28e91d2a89bf083bb2f643
soberkd <soberkd6@gmail.com>
2023-01-05 13:52:00 +0000

    Divide a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""A module that divides all elements of a matrix"""
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix.
+    Args:
+        matrix (:obj:`list` of :obj:`list`): The matrix to be divided.
+        div (int): The divisor number.
+    Returns:
+        list: A new matrix with all elements divided.
+    Raises:
+        TypeError: If `matrix` list of lists of integers or floats or
+            if `div` is not a number.
+        ZeroDivisionError: If `div` is equal to `0`.
+    """
+
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+
+    len_e = 0
+    msg_size = "Each row of the matrix must have the same size"
+
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError(msg_type)
+
+        if len_e != 0 and len(elems) != len_e:
+            raise TypeError(msg_size)
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError(msg_type)
+
+        len_e = len(elems)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)   
