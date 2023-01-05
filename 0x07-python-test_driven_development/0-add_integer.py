1ee61132d2086727cafe0c2479a59a05f63736a2
soberkd <soberkd6@gmail.com>
2023-01-05 07:56:24 +0000

    add two numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+""""A module that adds two numbers
+
+This module performs the addition operation on two numbers,
+these numbers can be integers of floats,
+if the numbers are floats they are converted ti integers.
+
+"""
 def add_integer(a, b=98):
-    """Add two integers"""
+    """Add two numbers"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
