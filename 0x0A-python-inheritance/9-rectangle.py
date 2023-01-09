04705dc9a27a224bcaec8820305738ff0fa9f986
soberkd <soberkd6@gmail.com>
2023-01-09 21:03:17 +0000

    full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Method """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle Class """
+    def __init__(self, width, height):
+        """ Instantiation method for rectangle class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """ I want it to work"""
+        i = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return(i)
+
+    def area(self):
+        """ I want to understand """
+        return self.__height * self.
