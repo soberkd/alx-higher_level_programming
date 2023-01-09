0757651ab6d2eba9e0d033b14486cc0a21ddb360
soberkd <soberkd6@gmail.com>
2023-01-09 12:57:56 +0000

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that check for same instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Module that check for same instance"""
+    return isinstance(obj, a_class)
