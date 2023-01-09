b40749f3008441b01b2a0e7b2fb1c393751685be
soberkd <soberkd6@gmail.com>
2023-01-09 11:10:56 +0000

    Print list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module MyList"""
+
+
+class MyList(list):
+    """Print sorted list"""
+
+    def print_sorted(self):
+        """Print sorted list"""
+
+        return print("{}".format(sorted(self)))
