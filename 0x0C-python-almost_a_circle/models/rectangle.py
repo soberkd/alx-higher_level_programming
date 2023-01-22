4a29cea4c41853231735eddca2e6387bb33a7695
soberkd <soberkd6@gmail.com>
2023-01-22 15:27:29 +0000

    Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,8 +114,9 @@ class Rectangle(Base):
     def __str__(self):
         """It returns a string representation of the Rectangle instance"""
 
-        return "[Rectangle] ({}) {}/{} - {}/{}\
-            ".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
 
     def update(self, *args, **kwargs):
         """It assigns an argument to each attribute
