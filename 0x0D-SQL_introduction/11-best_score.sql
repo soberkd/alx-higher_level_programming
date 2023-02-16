4846c2d9544d2fd09018ed12761fa92888935bee
soberkd <soberkd6@gmail.com>
2023-02-16 13:36:47 +0000

    lists all records with a score >= 10 in the table

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- lists all records with a score >= 10 in the table
+-- display both the score and the name (in this order)
+-- Records ordered by score (top first)
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
